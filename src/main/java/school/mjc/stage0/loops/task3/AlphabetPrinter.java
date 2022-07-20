b4bbe638555530e626dbf6221e0b207fcae23dce
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-20 14:47:45 +0400

    For Loops

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char  letter = 97;
+        for (int i = 97; i <= 122 ; i++){
+            letter = (char) i;
+            System.out.println(letter);
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+            int result = 0;
+            int n = 9;
+            for (int i = 1; i <=lengthOfLastNumber; i++){
+                result = result +n;
+                n = (n * 10) + 9;
+            }
+        System.out.println(result);
 
     }
 }
+
